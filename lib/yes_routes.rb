9a7cfa7efab8df00e8c24bc460bb9f010c76fd96
Sean Walker <swlkr.rbl@gmail.com>
2022-05-14 11:02:43 -0600

    Start adding rbs types

--- lib/yes_routes.rb
+++ lib/yes_routes.rb
@@ -1,19 +1,37 @@
 # frozen_string_literal: true
 
+require "yescode/refinements"
+
+module Strings
+  def snake_case(str)
+    str.gsub(/\B([A-Z])/, '_\1').downcase
+  end
+end
+
 class YesRoutes
   using Refinements
   class RouteDoesNotExistError < StandardError; end
   class RouteParamsNilError < StandardError; end
 
+  class YesRoute
+    attr_accessor :path, :class_name, :method_name
+
+    def initialize(path:, class_name:, method_name:)
+      @path = path
+      @class_name = class_name
+      @method_name = method_name
+    end
+  end
+
   class << self
     attr_accessor :routes, :paths
 
-    def match(verb, path_string, class_name, method_name)
+    def match(verb, path, class_name, method_name)
       @paths ||= {}
-      @paths[[class_name, method_name]] = path_string
+      @paths[[class_name, method_name]] = path
 
       @routes ||= Hash.new { |hash, key| hash[key] = [] }
-      @routes[verb] << [path_string, class_name, method_name]
+      @routes[verb] << YesRoute.new(path: path, class_name: class_name, method_name: method_name)
     end
 
     def path(class_name, method_name, params = {})
@@ -36,22 +54,23 @@ class YesRoutes
       match("POST", path_string, class_name, method_name)
     end
 
-    def resource(path_string, class_name)
-      snake_case = class_name.snake_case
-      [
-        [:get, "index", ""],
-        [:get, "new", "/new"],
-        [:post, "create", "/new"],
-        [:get, "show", "/:#{snake_case}"],
-        [:get, "edit", "/:#{snake_case}/edit"],
-        [:post, "update", "/:#{snake_case}/edit"],
-        [:post, "delete", "/:#{snake_case}/edit"]
-      ].each do |method, method_name, suffix|
-        public_send(method, [path_string, suffix].join("/").gsub(%r{/+}, "/"), class_name, method_name)
-      end
+    def resource(prefix, class_name)
+      snake_case = Strings.snake_case(class_name.to_s).gsub(/\B([A-Z])/, '_\1').downcase
+
+      match("GET", full_path(prefix, ""), class_name, :index)
+      match("GET", full_path(prefix, "/new"), class_name, :new)
+      match("POST", full_path(prefix, "/new"), class_name, :create)
+      match("GET", full_path(prefix, "/:#{snake_case}"), class_name, :show)
+      match("GET", full_path(prefix, "/:#{snake_case}/edit"), class_name, :edit)
+      match("POST", full_path(prefix, "/:#{snake_case}/edit"), class_name, :update)
+      match("POST", full_path(prefix, "/:#{snake_case}/delete"), class_name, :delete)
     end
 
     private
+
+    def full_path(prefix, suffix)
+      "#{prefix}#{suffix}".gsub(%r{/+}, "/")
+    end
 
     def sub_params_in_path(path, params = {})
       params.transform_keys!(&:inspect)
